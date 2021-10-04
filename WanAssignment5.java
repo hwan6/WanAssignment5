e852380e419346112c282a297e83a80db0622ced
Richard Wan <wanhe1993@gmail.com>
2021-10-03 22:31:07 -0500

    updated WanAssignment5 program to demontrate commits

--- WanAssignment5.java
+++ WanAssignment5.java
@@ -1,6 +1,6 @@
 public class WanAssignment5 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello WanAssignment5!");
+		System.out.println("Hello WanAssignment5! This is after the commit");
 	}
 }
